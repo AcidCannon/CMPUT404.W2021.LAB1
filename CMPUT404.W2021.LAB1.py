e786b5bca60a1fac954d395efd64ad4c789bfda1
Zijian Xi <zxi3@cs.ualberta.ca>
2021-01-11 18:50:08 -0700

    Should work.

--- CMPUT404.W2021.LAB1.py
+++ CMPUT404.W2021.LAB1.py
@@ -6,3 +6,8 @@ GOOGLE_HOMEPAGE_URL = "http://google.com/"
 
 req = requests.get(GOOGLE_HOMEPAGE_URL)
 print(req.text)
+
+THIS_FILE_URL = "https://raw.githubusercontent.com/AcidCannon/CMPUT404.W2021.LAB1/main/CMPUT404.W2021.LAB1.py"
+
+req = requests.get(THIS_FILE_URL)
+print(req.text)
